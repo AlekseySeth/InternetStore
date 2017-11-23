e02f60add3f3ab2f68d59d054e0b43e4ffdd38ed
Aleksey <aleksey.v.sh@gmail.com>
2017-11-23 15:46:13 +0300

    authorization logic

--- web/WEB-INF/classes/servlet/account/SignInServlet.java
+++ web/WEB-INF/classes/servlet/account/SignInServlet.java
@@ -1,10 +1,16 @@
 package servlet.account;
 
+import com.sun.corba.se.impl.resolver.ORBDefaultInitRefResolverImpl;
+import entity.order.Order;
+import entity.user.User;
+import service.AuthorizationService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static util.ServletUtil.getPath;
@@ -24,7 +30,17 @@ public class SignInServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        получает email & password и вызывает AuthorizationService.signIn если != null то createInitialOrder
-//        затем записывает данные user и order в сессию
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        HttpSession session = req.getSession();
+
+        AuthorizationService authorizationService = AuthorizationService.newInstance();
+        User user = authorizationService.signIn(email, password);
+
+        if (user != null) {
+            Order initialOrder = authorizationService.createInitialOrder(user);
+            session.setAttribute("user", user);
+            session.setAttribute("order", initialOrder);
+        }
     }
 }
